1bd2d3d1c0a0c1c358c5e032b04212764caded25
Galen Curwen-McAdams <galencm@fastmail.com>
2018-01-19 12:57:43 +0000

    Problem: ma-vis does nothing
    Solution: add initial cli logic and
    visualization code from previous test.
    Include a self-documentation feature to
    generate svg images of tool output for
    readme.

--- ma_cli/ma_vis.py
+++ ma_cli/ma_vis.py
@@ -3,3 +3,26 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 #
 # Copyright (c) 2018, Galen Curwen-McAdams
+
+import argparse
+from lings import pipeling
+from lings import routeling
+from ma_cli import visualize
+import subprocess
+
+def main():
+    """
+    Visualize state of running lings, services, machines...
+    """
+    parser = argparse.ArgumentParser(description=main.__doc__,formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument("--pattern", required=False,help="filter generated graph")
+    parser.add_argument("--self-document", action='store_true', required=False,help="generates sanitized svg for README")
+
+    args = parser.parse_args()
+
+    if args.self_document:
+        git_rev = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode()
+        title="ma-vis@{}".format(git_rev)
+        visualize.graph_display(file="ma-vis-screenshot", **{'sanitize':True,'title':title })
+
+    visualize.graph_display()
---
+++ ma_cli/visualize.py
@@ -0,0 +1,95 @@
+from lings import pipeling
+from lings import routeling
+import subprocess
+import zerorpc
+
+def graph_ecosystem(title="",sanitize=False):
+    graph = "digraph graphname {\n"
+    route_calls = []
+
+    routes = routeling.get_routes("*")
+    for route in routes:
+        for r in route.route_rules:
+            args = ""
+            if r.args:
+                for arg in r.args:
+                    args += arg.arg+"\n"
+            args = args.strip()
+
+            lcompare_string = "T"
+            rcompare_string = "T"
+            if r.left_compare:
+                print(r.left_compare.comparator_value,r.left_compare.comparator_symbol.symbol)
+                lcompare_string = "{0}{1}".format(r.left_compare.comparator_value,r.left_compare.comparator_symbol.symbol)
+            if r.right_compare:
+                print(r.right_compare.comparator_value,r.right_compare.comparator_symbol.symbol)
+                rcompare_string = "{1}{0}".format(r.right_compare.comparator_value,r.right_compare.comparator_symbol.symbol)
+
+            channel_node = r.channel.replace("/","FSLASH")
+
+            lnode = "{0}{1}".format(channel_node,hash(lcompare_string))
+            rnode = "{0}{1}".format(channel_node,hash(rcompare_string))
+            graph += '{0} [label="{1}"]\n'.format(channel_node,r.channel)
+            #graph += '{0} [label="{1}"]\n'.format(hash(args),args)
+            if r.action == "pipe":
+                graph += '{0} -> {1} [label="{2}"]\n'.format(channel_node,args.split("\n")[0],lcompare_string + " x " + rcompare_string + " "+args)
+            else:
+                graph += '{} -> {} [label="{}"]\n'.format(channel_node,r.action,lcompare_string + " x " + rcompare_string + " "+args)
+            route_calls.append(r.action)
+
+    pipes = pipeling.get_pipes("*")
+
+    for p in pipes:
+        print(p.name)
+        graph += '{0} [label="{0}"]\n'.format(p.name)
+        for step in p.pipe_steps:
+            print(step.call)
+            route_calls.append(step.call)
+            graph += '{0} [label="{0}"]\n'.format(step.call,step.call)
+            args = [arg.arg for arg in step.args]
+            graph += '{} -> {} [label="{}"]\n'.format(p.name,step.call,args)
+
+            #graph += '{0} [label="{0}"]\n'.format(step.call,step.call)
+        print(args)
+
+    for service in routeling.fuzzy_lookup('zerorpc-'):
+        try:
+            zc = zerorpc.Client()
+            print(service)
+            zc.connect("tcp://{}:{}".format(service['ip'],service['port']))
+            result = zc("_zerorpc_inspect")
+            #print(result)
+            for k,v in result['methods'].items():
+                if k in route_calls:
+                    #print(v['args'])
+                    if sanitize is True:
+                        ip = "".join([ s if s == "." else "x" for s in service['ip'] ])
+                        port = "".join([ s if s == "." else "x" for s in str(service['port']) ])
+                        graph += '{0} [label="{1}",style="dotted"]\n'.format("rpc"+k,k+"\n{}\n{}\n".format(ip,port))
+                    else:
+                        graph += '{0} [label="{1}",style="dotted"]\n'.format("rpc"+k,k+"\n{}\n{}\n".format(service['ip'],service['port']))
+
+                    graph += '{0} -> {1} [label="rpc",style="dotted"]\n'.format(k,"rpc"+k)
+
+        except Exception as ex:
+            print(ex)
+
+    # func -> rpc -> machine
+    graph+= 'labelloc="t";'
+    graph+= 'label="{}";'.format(title)
+    graph += "}"
+
+    return graph
+
+def graph_display(file=None,**kwargs):
+    graph = graph_ecosystem(**kwargs)
+
+    with open("/tmp/graph.dot","w+") as f:
+        f.write(graph)
+
+    subprocess.call("dot /tmp/graph.dot -Grankdir=LR -Tsvg -o /tmp/graph.svg".split(" "))
+
+    subprocess.call(["display","/tmp/graph.svg"])
+
+    if file is not None:
+        subprocess.call("dot /tmp/graph.dot -Grankdir=LR -Tsvg -o {}.svg".format(file).split(" "))
