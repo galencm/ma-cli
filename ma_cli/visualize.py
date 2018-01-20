c41828dd2f81b17319340bd5159da1a409cc2f7c
Galen Curwen-McAdams <galencm@fastmail.com>
2018-01-20 10:29:00 +0000

    Problem: ma-vis graph difficult to understand
    Solution: add graph key and color to highlight nodes

--- ma_cli/visualize.py
+++ ma_cli/visualize.py
@@ -4,6 +4,23 @@ import subprocess
 import zerorpc
 
 def graph_ecosystem(title="",sanitize=False):
+
+    graph_key = """
+    subgraph cluster_key {
+        label="Key";
+        fillcolor=blue
+        rpc_example[label="rpc\n[host]\n[port]", style="dotted"];
+        pipe_example[label="pipe", style=filled, fillcolor=lightskyblue3];
+        route_example[label="route", style=filled, fillcolor=lightskyblue1];
+        call_example[label="call"];
+        call2_example[label="another call"];
+        route_example->call_example[label="[left compare] [value] [right compare] [args..]",color=lightskyblue1];
+        pipe_example->call_example[label="[arg1, arg2 ... ]"]
+        pipe_example->call2_example[label=""]
+
+    }
+    """
+
     graph = "digraph graphname {\n"
     route_calls = []
 
@@ -29,19 +46,19 @@ def graph_ecosystem(title="",sanitize=False):
 
             lnode = "{0}{1}".format(channel_node,hash(lcompare_string))
             rnode = "{0}{1}".format(channel_node,hash(rcompare_string))
-            graph += '{0} [label="{1}"]\n'.format(channel_node,r.channel)
+            graph += '{0} [label="{1}", style=filled, fillcolor=lightskyblue1]\n'.format(channel_node,r.channel)
             #graph += '{0} [label="{1}"]\n'.format(hash(args),args)
             if r.action == "pipe":
-                graph += '{0} -> {1} [label="{2}"]\n'.format(channel_node,args.split("\n")[0],lcompare_string + " x " + rcompare_string + " "+args)
+                graph += '{0} -> {1} [label="{2}", color=lightskyblue1]\n'.format(channel_node,args.split("\n")[0],lcompare_string + " x " + rcompare_string + " "+args)
             else:
-                graph += '{} -> {} [label="{}"]\n'.format(channel_node,r.action,lcompare_string + " x " + rcompare_string + " "+args)
+                graph += '{} -> {} [label="{}", color=lightskyblue1]\n'.format(channel_node,r.action,lcompare_string + " x " + rcompare_string + " "+args)
             route_calls.append(r.action)
 
     pipes = pipeling.get_pipes("*")
 
     for p in pipes:
         print(p.name)
-        graph += '{0} [label="{0}"]\n'.format(p.name)
+        graph += '{0} [label="{0}", style=filled, fillcolor=lightskyblue3]\n'.format(p.name)
         for step in p.pipe_steps:
             print(step.call)
             route_calls.append(step.call)
@@ -77,6 +94,8 @@ def graph_ecosystem(title="",sanitize=False):
     # func -> rpc -> machine
     graph+= 'labelloc="t";'
     graph+= 'label="{}";'.format(title)
+
+    graph += graph_key
     graph += "}"
 
     return graph
