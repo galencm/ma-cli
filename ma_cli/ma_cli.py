7a5b8894678a5cdad419d221a0e66da206ba6403
Galen Curwen-McAdams <galencm@fastmail.com>
2018-01-22 19:47:58 +0000

    Problem: checking scheduler status repetitive
    Solution: add cli for basic use in ma-cli

--- ma_cli/ma_cli.py
+++ ma_cli/ma_cli.py
@@ -12,6 +12,88 @@ from functools import wraps,partialmethod
 import paho.mqtt.client as mosquitto
 from ma_cli import local_tools
 
+class NomadCLI(Cmd):
+    """Given a host and port attempts to connect as client to zerorpc
+    server. On connection gets list of remote services and dynamically
+    generates attributes to allow tab completion.
+    """
+    def __init__(self, host,port):
+        #disable,otherwise argparse parsers in main() will interact poorly
+        self.allow_cli_args = False
+        self.redirector = '--->'
+        self.allow_redirection = False
+        self.host = host
+        self.port = "4646"
+        Cmd.__init__(self)
+
+    @property
+    def scheduler_address(self):
+        return "-address=http://{}:{}".format(self.host,self.port)
+
+    def call_scheduler(self,call_string,subs,raw=False):
+        subs.update({"address" : self.scheduler_address})
+        call_string = call_string.format(**subs)
+        output = subprocess.check_output(call_string.split(" ")).decode()
+        print(output)
+        if raw:
+            return output
+
+    def do_status(self,args):
+        self.call_scheduler("nomad status {address}",{})
+
+    def do_logs(self, job_id):
+        subs =  {k : v for k, v in locals().items() if not k == 'self'}
+        retry_with_id = False
+        try:
+            self.call_scheduler("nomad logs {address} {job_id}" ,subs)
+            self.call_scheduler("nomad logs -stderr {address} {job_id}" ,subs)
+        except:
+            # Try to scrape for job id
+            # this will not work well if a job
+            # has multiple ids
+            job_id = self.call_scheduler("nomad job status {address} {job_id}" ,subs,raw=True)
+            id_line = False
+            for line in job_id.split("\n"):
+                print(">>",line)
+                if id_line:
+                    # overwrite subs jobs_id with scraped
+                    subs['job_id'] = line.split(" ",1)[0]
+                    retry_with_id = True
+                    break
+                if "Node ID" in line:
+                    id_line = True
+
+        if retry_with_id is True:
+            self.call_scheduler("nomad logs {address} {job_id}" ,subs)
+            self.call_scheduler("nomad logs -stderr {address} {job_id}" ,subs)
+
+    def do_job(self,job_name):
+        subs =  {k : v for k, v in locals().items() if not k == 'self'}
+        self.call_scheduler("nomad job status {address} {job_name}" ,subs)
+
+    def do_start(self,job_name):
+        # alias for run
+        # assumes running on machine that job files
+        # were generated on.
+        # expects generated job files in:
+        # ~/.local/jobs
+        import os
+        subs =  {k : v for k, v in locals().items() if not k == 'self'}
+        job_file  = os.path.join(os.path.expanduser('~'),".local/jobs","{}.hcl".format(job_name))
+        print(job_file)
+        if os.path.isfile(job_file):
+            subs['job_file'] = job_file
+            self.call_scheduler("nomad run {address} {job_file}" ,subs)
+        else:
+            print("not found: {job_file}".format(**subs))
+
+    def do_stop(self,job_name):
+        subs =  {k : v for k, v in locals().items() if not k == 'self'}
+        self.call_scheduler("nomad stop {address} {job_name}" ,subs)
+
+    def do_purge(self,job_name):
+        subs =  {k : v for k, v in locals().items() if not k == 'self'}
+        self.call_scheduler("nomad job stop -purge {address} {job_name}" ,subs)
 
 class ZeroRpcCLI(Cmd):
     """Given a host and port attempts to connect as client to zerorpc
@@ -102,6 +184,12 @@ def cli_mqtt(ip,port):
     c = MqttCLI(ip,port)
     c.cmdloop()
 
+def cli_nomad(ip,port):
+
+    print("nomad-cli...")
+    c = NomadCLI(ip,port)
+    c.cmdloop()
+
 def main():
     """
     terminals and repls for different services
@@ -109,7 +197,7 @@ def main():
 
     parser = argparse.ArgumentParser(description=main.__doc__,formatter_class=argparse.RawDescriptionHelpFormatter)
     parser.add_argument("service", default=None, nargs='?', help="service name to connect to")
-    parser.add_argument("--cli", choices=("redis","mqtt","zerorpc"), help="cli type")
+    parser.add_argument("--cli", choices=("redis","mqtt","zerorpc","nomad"), help="cli type")
     parser.add_argument("--info", action='store_true', help="print service info and quit")
 
     args = parser.parse_args()
@@ -146,12 +234,20 @@ def main():
     if args.service == "redis" or args.cli == "redis":
         try:
             cli_redis(ip,port)
-        except:
+        except Exception as ex:
+            print(ex)
+            cli_zerorpc(ip,port)
+    elif args.service == "nomad" or args.cli == "nomad":
+        try:
+            cli_nomad(ip,port)
+        except Exception as ex:
+            print(ex)
             cli_zerorpc(ip,port)
     elif args.service == "mqtt" or args.cli == "mqtt":
         try:
             cli_mqtt(ip,port)
-        except:
+        except Exception as ex:
+            print(ex)
             cli_zerorpc(ip,port)
     else:
         cli_zerorpc(ip,port)
